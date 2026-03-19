af83647090b3ecd07608cba5b6b7e91e1738e92f
crespif <crespif@celta.com.ar>
2026-03-19 13:02:29 -0300

    se agregan url de comprobacioon para nave

--- app/checkout/nave/page.tsx
+++ app/checkout/nave/page.tsx
@@ -2,12 +2,9 @@
 "use client";
 
 import { CheckCircle } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
-import { use, useEffect } from 'react';
-
-import io from 'socket.io-client';
-import { toast } from "@/hooks/use-toast";
-const socket = io(process.env.NEXT_PUBLIC_URL!);
+//import io from 'socket.io-client';
+//import { toast } from "@/hooks/use-toast";
+//const socket = io(process.env.NEXT_PUBLIC_URL!);
 
 export default function SuccessPage() {
 
@@ -18,9 +15,6 @@ export default function SuccessPage() {
                 <CheckCircle className="text-green-500 w-16 h-16 mb-4" />
             </div>
             <p className="mb-2">Tu pedido ha sido procesado con éxito.</p>
-            {/* <p className="mb-2">ID de Pago: {payment_id}</p>
-      <p className="mb-2">Estado: {status}</p>
-      <p className="mb-2">Referencia Externa: {external_reference}</p> */}
             <p className="mb-2">Una vez verificado el pago, comenzaremos a preparar tu pedido.</p>
             <p className="mt-4">Nos pondremos en contacto contigo a la brevedad.</p>
             <p className="mt-4">¡Gracias por elegir Uzcudun Coffee Ride!</p>
